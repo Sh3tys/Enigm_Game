c5f5ab76b222dc605a3f058d9a058bba5f6ca782
Sh3tys <titouanparzival@gmail.com>
2025-01-12 00:42:33 +0100

    Add new folder name song for the hub.html and design the button for play or stop the song

--- script/scriptHub.js
+++ script/scriptHub.js
@@ -36,4 +36,22 @@ submitCodeButton.addEventListener('click', function() {
     }
 });
 
+const audio = document.getElementById("myAudio");
+const button = document.getElementById("audioControl");
+const buttonImage = document.getElementById("buttonImage");
+
+
+button.addEventListener("click", () => {
+ 
+  if (audio.paused) {
+    audio.play();
+    buttonImage.src = "./picture/song on.png"; 
+  } 
+
+  else {
+    audio.pause();
+    buttonImage.src = "./picture/song off.png";
+  }
+});
+
      
