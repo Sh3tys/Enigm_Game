2331dc77fee2746c6996b525090aeb7312d94e70
Sh3tys <titouanparzival@gmail.com>
2025-01-12 20:14:50 +0100

    Create 2 steps with history

--- script/scriptChapter1.js
+++ script/scriptChapter1.js
@@ -1,12 +1,54 @@
 
-const etape1 = document.getElementById('resultStep1');
-const btn1 = document.getElementById('btn');
+const resultEtape1 = document.getElementById('resultStep1');
+const resultEtape2 = document.getElementById('resultStep2');
+const etape2 = document.getElementById('etape2');
+const etape3 = document.getElementById('etape3');
+//const etape4 = document.getElementById('resultStep4');
+//const etape5 = document.getElementById('resultStep5');
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+//const btn3 = document.getElementsByName('btn3');
+//const btn4 = document.getElementsByName('btn4');
+//const btn5 = document.getElementsByName('btn5');
 
-btn1.addEventListener('click',function() {
-    if (etape1.value === 'code') {
+
+btn1.addEventListener('click',function(){
+    if (resultEtape1.value === '16') {
+        etape2.style.display = 'block';
+        resultEtape1.readOnly = true; // retire la modification possible
+        btn1.style.display = 'none';
+    } else {
+        resultEtape1.value = '';
+    }
+});
+
+btn2.addEventListener('click',function() {
+    if (resultEtape2.value === '7, -124') {
+        etape3.style.display = 'block';
+        resultEtape2.readOnly = true; // retire la modification possible
+        btn2.style.display = 'none';
+    } else {
+        resultEtape2.value = '';
+    }
+});
+/*
+btn3.addEventListener('click',function() {
+    if (etape1.value === '16') {
         etape2.style.display = 'block';
         etape1.readOnly = true; // retire la modification possible
+        btn3.style.display = 'none';
     } else {
         etape1.value = '';
     }
 });
+
+btn4.addEventListener('click',function() {
+    if (etape1.value === '16') {
+        etape2.style.display = 'block';
+        etape1.readOnly = true; // retire la modification possible
+        btn4.style.display = 'none';
+    } else {
+        etape1.value = '';
+    }
+});
+*/
